15bd6bc7756e6026a30f40d2c0b9354d13a242da
Tono Abad <110565791+tono2002@users.noreply.github.com>
2023-05-29 02:50:26 +0200

    Update 238.ProductOfArrayExceptSelf.java

--- 238.ProductOfArrayExceptSelf.java
+++ 238.ProductOfArrayExceptSelf.java
@@ -3,34 +3,48 @@ class Solution {
     // Time complexity: 3 loops * n iterations = 3n = 0(n)
     // Space complexity: 0(n): We use 3 arrays of size n
     
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] nums){
         int length = nums.length;
+        
+        int[] leftProduct = new int[length];
+        int[] rightProduct = new int[length];
+        int[] output = new int[length];
 
-    int[] leftProduct = new int[length]; // Product of all numbers to the left
-    int[] rightProduct = new int[length]; // Product of all numbers to the right
-    int[] output = new int[length]; // Final product array
+        leftProduct[0] = 1; // The first element has no product in its left.
+        rightProduct[length - 1] = 1; // The last element has no product in its right.
 
-    // Left most element of leftProduct array is always 1
-    leftProduct[0] = 1;
+        // Left product of each element
+        for(int i = 1; i < length ; i++){
+            leftProduct[i] = nums[i - 1] * leftProduct[i - 1];
+        }
 
-    // Rightmost most element of rightProduct array is always 1
-    rightProduct[length - 1] = 1;
+        // Right product of each element
+        for(int i = length - 2; i >= 0; i--){
+            rightProduct[i] = nums[i + 1] * rightProduct[i + 1];
+        }
 
-    // Construct the leftProduct array
-    for (int i = 1; i < length; i++) {
-        leftProduct[i] = nums[i - 1] * leftProduct[i - 1];
+        // Multiply left and right product arrays
+        for(int i = 0; i < length; i++){
+            output[i] = leftProduct[i] * rightProduct[i];
+        }
+
+        return output;
     }
 
-    // Construct the rightProduct array
-    for (int i = length - 2; i >= 0; i--) {
-        rightProduct[i] = nums[i + 1] * rightProduct[i + 1];
-    }
+    // Alternative solution, inefficient
 
-    // Multiply leftProduct and rightProduct array
-    for (int i = 0; i < length; i++) {
-        output[i] = leftProduct[i] * rightProduct[i];
-    }
+    // Time complexity: 0(n^2): Two nested loops
+    // Space complexity: 0(n), due to the output array
 
-    return output;
+    public int[] productExceptSelf2(int[] nums){
+        int[] output = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            int temp = 1;
+            for(int j = 0; j < nums.length; j++){
+                if(i != j) temp = temp * nums[j];
+            }
+            output[i] = temp;
+        }
+        return output;
     }
 }
